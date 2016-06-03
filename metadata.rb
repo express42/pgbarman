e5c972a5d53ebe5a3a53ed74c3361c74102e68f7
Sergey Bogatyrets <serj.sj@gmail.com>
2016-06-04 01:06:31 +0300

    Compability with chef11

--- metadata.rb
+++ metadata.rb
@@ -1,8 +1,8 @@
 name 'pgbarman'
 maintainer 'Express 42'
 maintainer_email 'cookbooks@express42.com'
-issues_url 'https://github.com/express42-cookbooks/pgbarman/issues'
-source_url 'https://github.com/express42-cookbooks/pgbarman'
+issues_url 'https://github.com/express42-cookbooks/pgbarman/issues' if respond_to?(:issues_url)
+source_url 'https://github.com/express42-cookbooks/pgbarman' if respond_to?(:source_url)
 license 'all_rights'
 description 'Installs/Configures pgbarman'
 long_description 'Installs/Configures pgbarman'
